89cfde2e216ecbea50b6c19ae26e1f5235c059d7
techie-explorer <farhan_mohammed98@outlook.com>
2020-02-17 04:39:30 +0530

    Now testing using HTML

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def say_hello():
     return 'Hello Farhan! This is direct integration from GitHub' \
-           '<div style="height=50px; width=50px; background-color=red"></div>'
+           '<div style="height: 50px; width: 50px;"></div>'
 
 
 if __name__ == '__main__':
