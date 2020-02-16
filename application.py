c35cd9b4eae1f45c6b5bbb293a105c0fb8180a5e
techie-explorer <farhan_mohammed98@outlook.com>
2020-02-17 04:37:26 +0530

    Testing live changes from GitHub to AppService

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return "Hello Farhan! This is direct integration from GitHub"
+    return 'Hello Farhan! This is direct integration from GitHub' \
+           '<div style="height=50px; width=50px; background-color=red"></div>'
 
 
 if __name__ == '__main__':
