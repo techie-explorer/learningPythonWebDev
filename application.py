612adfa7f769505edeff344601f7b13808d7cc6b
techie-explorer <farhan_mohammed98@outlook.com>
2020-02-17 04:50:18 +0530

    Now testing using HTML part 2

--- application.py
+++ application.py
@@ -3,10 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def render_homepage():
+    render_greeting_text = """
+    <div style="height: 100px; width: 100px; background-color: 'red'">
+        Hello Welcome to Azure Python Flask Application
+    </div>
+    """
+    return render_greeting_text
+
+
 @app.route('/')
 def say_hello():
-    return 'Hello Farhan! This is direct integration from GitHub' \
-           '<div style="height: 50px; width: 50px;"></div>'
+    return render_homepage()
+
+
+def render_details_page():
+    details_page = "hahahha"
+    return details_page
+
+
+@app.route('details')
+def say_hello():
+    return render_details_page()
 
 
 if __name__ == '__main__':
